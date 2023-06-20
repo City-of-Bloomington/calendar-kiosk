367903cb4a72b200e44f706453ed772d09486c24
Cliff Ingham <inghamn@bloomington.in.gov>
2023-06-20 17:12:10 -0400

    Scrolling div should make room for footer
    Allow for footer space when setting the size of upcoming_events.  Footer is absolutly positioned at the bottom of the screen.  If no space is accounted for, then upcoming_meetings overlap the footer.
    
    Updates #7

--- data/Themes/COB/public/js/kiosk.js
+++ data/Themes/COB/public/js/kiosk.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
-    const div        = document.getElementById('upcoming_meetings');
-    const inc        = 4;
-    let   needed     = 1920 - div.offsetTop; // 864
-    div.style.height = needed + 'px';
+    const div    = document.getElementById('upcoming_meetings');
+    const inc    = 4;
 
     function scroll()
     {
@@ -10,6 +8,11 @@ $(document).ready(function () {
         $("#upcoming_meetings").scrollTop(div.scrollTop + inc);
     }
     if (window.innerWidth == 1080 && window.innerHeight == 1920) {
+        let needed = 1920
+                   -  140 // Footer
+                   - div.offsetTop;
+
+        div.style.height = needed + 'px';
         setInterval(scroll, 500);
     }
 });
